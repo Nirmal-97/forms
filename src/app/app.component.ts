0f89ace5e0109215a465f52155b39087082f25ad
Nirmal Kumar <84436580+Nirmal-Kumar97@users.noreply.github.com>
2022-09-01 17:46:58 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'forms';
+  @ViewChild('f') signupForm: NgForm | undefined;
+  deafultQuestion = 'pet';
+  answer = '';
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
